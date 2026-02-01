ba5f080df92be4759efacdccc7b2041b1fb31436
Mr. Triwitch <83054137+Eleazar4628@users.noreply.github.com>
2026-02-01 18:43:01 -0300

    Bump version from 1.0 to 1.1 in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="ytmd",
-    version="1.0",
+    version="1.1",
     py_modules=["ytmd"],
     install_requires=[
         "yt-dlp",
@@ -13,4 +13,5 @@ setup(
             'ytmd=ytmd:main',
         ],
     },
+
 )
