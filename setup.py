5b44e00bd7ac3927e3cf7dd9ea4a533286b33808
Mr. Triwitch <83054137+Eleazar4628@users.noreply.github.com>
2026-07-25 16:22:03 -0300

    Bump version from 1.5.3.1 to 1.5.3.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="ytmd",
-    version="1.5.3.1",
+    version="1.5.3.2",
     py_modules=["ytmd"],
     python_requires=">=3.7",
     install_requires=[
